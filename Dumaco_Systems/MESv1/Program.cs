e084baeba527bd0457713dba1d56ed11e4b2e883
SanderBaan1 <74295294+SanderBaan1@users.noreply.github.com>
2024-03-28 09:31:29 +0100

    Add ERP refference and test code

--- Dumaco_Systems/MESv1/Program.cs
+++ Dumaco_Systems/MESv1/Program.cs
@@ -1,10 +1,14 @@
-﻿namespace MESv1
+﻿using Ridder.Client.SDK;
+
+namespace MESv1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            RidderIQSDK application = new RidderIQSDK();
+            ISDKResult loginResult = application.Login("Username", "Password", "Company");
         }
     }
 }
